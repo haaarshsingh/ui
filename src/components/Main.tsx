9ba901eda59811c24e092a388da3ad1f86b75f0f
Harsh Singh <hi.harsh@protonmail.ch>
2024-09-13 03:13:38 -0400

    init

---
+++ src/components/Main.tsx
@@ -0,0 +1,15 @@
+export default () => {
+  return (
+    <main className='flex'>
+      <div>
+        <h2>(Re)Creations</h2>
+      </div>
+      <div>
+        <h2>Components</h2>
+      </div>
+      <div>
+        <h2>Miscellaneous</h2>
+      </div>
+    </main>
+  )
+}
